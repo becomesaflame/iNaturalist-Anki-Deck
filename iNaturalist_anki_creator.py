79080c0af5d79e8e61b7b22ba6afcf86a1f22b31
Spark <spark.scul@gmail.com>
2023-08-29 15:31:20 -0400

    Put back the feature that downloads photos and includes them in the package

--- iNaturalist_anki_creator.py
+++ iNaturalist_anki_creator.py
@@ -1,7 +1,7 @@
 import requests
 import genanki
-import re
 import os
+import urllib.request
 
 # Function to get taxon data from iNaturalist
 def get_taxon_data(taxon_id):
@@ -9,13 +9,15 @@ def get_taxon_data(taxon_id):
     data = response.json()
     return data['results'][0]
 
-# Function to get taxon photos from iNaturalist
-def get_taxon_photos(taxon_photos):
-    photos = ''
-    for photo in taxon_photos:
+# Function to download taxon photos and get local file paths
+def download_taxon_photos(taxon_photos):
+    photo_paths = []
+    for i, photo in enumerate(taxon_photos):
         url = photo['photo']['medium_url']
-        photos += f'<img src="{url}"><br>'
-    return photos
+        photo_path = f'taxon_{i}.jpg'
+        urllib.request.urlretrieve(url, photo_path)
+        photo_paths.append(photo_path)
+    return photo_paths
 
 # Function to get observation data from iNaturalist
 def get_observations(taxon_id, num_observations):
@@ -23,13 +25,15 @@ def get_observations(taxon_id, num_observations):
     data = response.json()
     return data['results']
 
-# Function to get observation photos from iNaturalist
-def get_observation_photos(observation_photos):
-    photos = ''
-    for photo in observation_photos:
+# Function to download observation photos and get local file paths
+def download_observation_photos(observation_photos):
+    photo_paths = []
+    for i, photo in enumerate(observation_photos):
         url = photo['url']
-        photos += f'<img src="{url}"><br>'
-    return photos
+        photo_path = f'observation_{i}.jpg'
+        urllib.request.urlretrieve(url, photo_path)
+        photo_paths.append(photo_path)
+    return photo_paths
 
 # Define the Anki model
 def create_model(num_observations):
@@ -78,6 +82,9 @@ while True:
         break
     taxon_urls.append(taxon_url)
 
+# Create media files list
+media_files = []
+
 # Process each taxon URL
 for taxon_url in taxon_urls:
     # Extract the taxon ID from the URL
@@ -86,19 +93,26 @@ for taxon_url in taxon_urls:
     # Fetch taxon data from iNaturalist
     taxon_data = get_taxon_data(taxon_id)
 
-    common_name = taxon_data['common_name']['name']
+    common_name = taxon_data['preferred_common_name']
     scientific_name = taxon_data['name']
     wikipedia_summary = taxon_data['wikipedia_summary']
     custom_description = ''
     inaturalist_id = taxon_data['id']
-    taxon_photos = get_taxon_photos(taxon_data['taxon_photos'])
+    taxon_photo_paths = download_taxon_photos(taxon_data['taxon_photos'])
+    media_files.extend(taxon_photo_paths)
+
+    # Create HTML for taxon photos
+    taxon_photos_html = '<br>'.join([f'<img src="{path}">' for path in taxon_photo_paths])
 
     # Fetch observation data from iNaturalist
     observations = get_observations(taxon_id, num_observations)
 
-    observation_photos = []
+    # Create HTML for observation photos
+    observation_photos_html = []
     for observation in observations:
-        observation_photos.append(get_observation_photos(observation['photos']))
+        observation_photo_paths = download_observation_photos(observation['photos'])
+        media_files.extend(observation_photo_paths)
+        observation_photos_html.append('<br>'.join([f'<img src="{path}">' for path in observation_photo_paths]))
 
     # Create the Anki note
     model = create_model(num_observations)
@@ -108,10 +122,10 @@ for taxon_url in taxon_urls:
         wikipedia_summary,
         custom_description,
         str(inaturalist_id),
-        taxon_photos,
+        taxon_photos_html,
     ]
-    for observation_photo in observation_photos:
-        fields.append(observation_photo)
+    for obs_photos_html in observation_photos_html:
+        fields.append(obs_photos_html)
 
     note = genanki.Note(
         model=model,
@@ -122,6 +136,8 @@ for taxon_url in taxon_urls:
     deck.add_note(note)
 
 # Save the deck to a file
-genanki.Package(deck).write_to_file('iNaturalistDeck.apkg')
+package = genanki.Package(deck)
+package.media_files = media_files
+package.write_to_file('iNaturalistDeck.apkg')
 
 print("Anki deck created successfully!")
